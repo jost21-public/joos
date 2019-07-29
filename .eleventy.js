5e0c9e37a858b635adef22924b2b90f5a5416d8b
Unknown <stingray21@gmx.de>
2019-07-29 12:16:05 +0200

    rearranged files again

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
 module.exports = function(config) {
 
-	config.addCollection('posts', collection => {
-		return collection.getFilteredByGlob('src/site/posts/*.md');
+	config.addPassthroughCopy("./site/images");
+	config.addPassthroughCopy("./admin");
+
+	config.addCollection("posts", collection => {
+		return collection.getFilteredByGlob("src/site/posts/*.md");
 	});
 
 	return {
 		dir: {
-			input: ".",
+			input: "./site",
 			output: "dist",
 			includes: "templates",
 			data: "_data"
 		},
-		templateFormats: ["html", "md", "css", "yml"],
+		templateFormats: ["html", "md", "css"],
 		htmlTemplateEngine: "liquid",
 		markdownTemplateEngine: "liquid"
 	};
-}
+};
